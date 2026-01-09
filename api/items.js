d4ff6f33d2f9c314c638e112beaf779d2a8720ff
Douglas Ribeiro Sanguino <86693183+DouglasRibeiroSanguino@users.noreply.github.com>
2026-01-09 13:03:33 +0000

    added new folder on this repo

--- api/items.js
+++ api/items.js
@@ -2,12 +2,12 @@ import fs from 'fs';
 import path from 'path';
 
 export default function handler(req, res) {
-  const filePath = path.join(process.cwd(), 'dataItems.json');
+  const filePath = path.join(process.cwd(), 'public', 'dataItems.json');
+
   const raw = fs.readFileSync(filePath, 'utf-8');
   const data = JSON.parse(raw);
 
   const { search, active } = req.query;
-
   let result = data.items;
 
   if (search) {
